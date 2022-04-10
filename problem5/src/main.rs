194dac419cd6bd7af00c1b01aa95a83f880883f4
Don Franke <franke.don@gmail.com>
2022-04-10 09:10:30 -0500

    Faster than 44.83% of Rust online submissions

--- problem5/src/main.rs
+++ problem5/src/main.rs
@@ -7,6 +7,7 @@ struct Solution {
 impl Solution {
     pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
         let mut highestbalance = 0;
+        
         for (_, account) in accounts.iter().enumerate() {
             let mut k = 0;
             for (_, balance) in account.iter().enumerate() {
@@ -16,6 +17,7 @@ impl Solution {
                 highestbalance = k;
             }
         }
+        
         highestbalance
     }
 }
