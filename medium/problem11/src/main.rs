52ba545b549d118280b42d8eb95308cc2f38eda6
Don Franke <franke.don@gmail.com>
2022-05-04 21:59:51 -0500

    Moved tests to separate file

--- medium/problem11/src/main.rs
+++ medium/problem11/src/main.rs
@@ -21,45 +21,4 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test1() {
-        // Input: n = "32"
-        // Output: 3
-        // Explanation: 10 + 11 + 11 = 32
-
-        let s: String = "32".to_string();
-        let target = 3;
-
-        let ret = Solution::min_partitions(s);
-
-        assert!(ret == target, "item 1 {} ", ret);
-    }
-
-    #[test]
-    fn test2() {
-        // Input: n = "82734"
-        // Output: 8
-        let s: String = "82734".to_string();
-        let target = 8;
-
-        let ret = Solution::min_partitions(s);
-
-        assert!(ret == target, "item 2 {} ", ret);
-    }
-
-    #[test]
-    fn test3() {
-        // Input: n = "27346209830709182346"
-        // Output: 9
-
-        let s: String = "27346209830709182346".to_string();
-        let target = 9;
-
-        let ret = Solution::min_partitions(s);
-
-        assert!(ret == target, "item 3 {} ", ret);
-    }
-}
+mod test;
