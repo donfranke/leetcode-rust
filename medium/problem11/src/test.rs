f8427ca4c76ba91326ac5365eeb528b7caef6d40
Don Franke <franke.don@gmail.com>
2022-05-04 22:48:20 -0500

    Fixed tests

--- medium/problem11/src/test.rs
+++ medium/problem11/src/test.rs
@@ -1,5 +1,4 @@
 #[cfg(test)]
-
 mod tests {
 
     use super::super::*;
@@ -12,19 +11,18 @@ mod tests {
 
         let s: String = "32".to_string();
         let target = 3;
+        let ret = Solution::min_partitions(s);
 
-        let ret = Solution {};
-
-        //assert!(ret.min_partitions(s) == target, "item 1 {} ", ret);
+        assert!(ret == target, "item 1 {} ", ret);
     }
 
     #[test]
     fn test2() {
         // Input: n = "82734"
         // Output: 8
+
         let s: String = "82734".to_string();
         let target = 8;
-
         let ret = Solution::min_partitions(s);
 
         assert!(ret == target, "item 2 {} ", ret);
@@ -37,7 +35,6 @@ mod tests {
 
         let s: String = "27346209830709182346".to_string();
         let target = 9;
-
         let ret = Solution::min_partitions(s);
 
         assert!(ret == target, "item 3 {} ", ret);
